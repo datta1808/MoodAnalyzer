8436fd3bdb53629f349fa13521d6de0e45cd157b
datta1808 <bagaledatta18@gmail.com>
2021-05-30 23:10:06 +0530

    Welcome to Mood Analyzer

---
+++ src/test/java/MoodAnalyzerTest.java
@@ -0,0 +1,14 @@
+import com.bridgelabz.MoodAnalyzerMain;
+import org.junit.Test;
+
+
+import static org.junit.Assert.*;
+
+public class MoodAnalyzerTest {
+    @Test
+    public void given_AnyMood_Should_Return_HAPPY() {
+        MoodAnalyzerMain moodAnalyser = new MoodAnalyzerMain();
+        String mood = moodAnalyser.analyseMood("I am in Any Mood");
+        assertEquals("HAPPY", mood);
+    }
+}
