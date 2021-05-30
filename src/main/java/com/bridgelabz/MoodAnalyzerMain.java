73213a4bf5673376f23c3cdc78e0f3357ec0a0ad
datta1808 <bagaledatta18@gmail.com>
2021-05-30 23:25:28 +0530

    ADDED: functionality to return SAD mood if give SAD mood

--- src/main/java/com/bridgelabz/MoodAnalyzerMain.java
+++ src/main/java/com/bridgelabz/MoodAnalyzerMain.java
@@ -1,12 +1,17 @@
 package com.bridgelabz;
 
 public class MoodAnalyzerMain {
-    // Constructor
+    private String message;
+
+    //Constructor
     public MoodAnalyzerMain() {
     }
 
-    public String analyseMood(String message) {
-        if (message.contains("Sad"))
+    public MoodAnalyzerMain(String message) {
+        this.message=message;
+    }
+    public String analyseMood() {
+        if (this.message.contains("Sad"))
             return "SAD";
         else
             return "HAPPY";
--- src/test/java/MoodAnalyzerTest.java
+++ src/test/java/MoodAnalyzerTest.java
@@ -5,9 +5,9 @@ import static org.junit.Assert.*;
 
 public class MoodAnalyzerTest {
     @Test
-    public void given_AnyMood_Should_Return_HAPPY() {
-        MoodAnalyzerMain moodAnalyser = new MoodAnalyzerMain();
-        String mood = moodAnalyser.analyseMood("I am in Any Mood");
-        assertEquals("HAPPY", mood);
+    public void given_SadMood_Should_Return_SAD() {
+        MoodAnalyzerMain moodAnalyser = new MoodAnalyzerMain("I am in Sad Mood");
+        String mood = moodAnalyser.analyseMood();
+        assertSame("SAD", mood);
     }
 }
